65f7b1fe57da92d449699b95f531048e5d08c455
thepearson <thepearson@gmail.com>
2013-01-18 16:17:30 +1300

    Fixed js issues

--- sites/all/themes/cet/template.php
+++ sites/all/themes/cet/template.php
@@ -8,7 +8,6 @@ function cet_link($variables) {
 }
 
 function cet_js_alter(&$javascript) {
-  $javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'cet') . '/js/jquery.js';
-  $javascript['misc/jquery.js']['scope'] = 'header';
-  $javascript['misc/jquery.js']['type'] = 'file';
+  $file = drupal_get_path('theme', 'cet') . '/js/jquery.js';
+  $javascript['misc/jquery.js']['data'] = drupal_js_defaults($file);
 }
