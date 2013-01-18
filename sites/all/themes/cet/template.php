545842ec2dc08936cab1b5244370ae88f3df26a3
Stefen Cook <stefen.cook@webessentials.co.nz>
2013-01-18 16:42:56 +1300

    Fix Craigs issue!

--- sites/all/themes/cet/template.php
+++ sites/all/themes/cet/template.php
@@ -9,5 +9,6 @@ function cet_link($variables) {
 
 function cet_js_alter(&$javascript) {
   $file = drupal_get_path('theme', 'cet') . '/js/jquery.js';
-  $javascript['misc/jquery.js'] = drupal_js_defaults($file);
+  $javascript['misc/jquery.js']['data'] = $file;
+  $javascript['misc/jquery.js']['scope'] = 'header';
 }
