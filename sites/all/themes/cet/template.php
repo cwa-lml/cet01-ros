db318891269b1185afa0e3c6be2abf279d7cb59a
Richard Parratt <rich.d.rich@gmail.com>
2013-01-23 11:05:04 +1300

    Customise text on username and forgot password screens

--- sites/all/themes/cet/template.php
+++ sites/all/themes/cet/template.php
@@ -12,3 +12,43 @@ function cet_js_alter(&$javascript) {
   $javascript['misc/jquery.js']['data'] = $file;
   $javascript['misc/jquery.js']['scope'] = 'header';
 }
+
+function cet_theme(&$existing, $type, $theme, $path) {
+	$items = array();
+    
+  $items['user_login'] = array(
+    'render element' => 'form',
+    'path' => drupal_get_path('theme', 'cet') . '/bootstrap/bootstrap/templates',
+    'template' => 'user-login',
+    'preprocess functions' => array(
+       'cet_preprocess_user_login'
+    ),
+  );
+
+  $items['user_pass'] = array(
+      'render element' => 'form',
+      'path' => drupal_get_path('theme', 'cet') . '/bootstrap/bootstrap/templates',
+      'template' => 'user-pass',
+      'preprocess functions' => array(
+         'cet_preprocess_user_pass'
+  ),
+  );
+  
+  return $items;
+}
+
+function cet_preprocess_user_login(&$variables) {
+  $variables['post_text'] = t('If you have purchased the Resource kit, an email has been sent to the Office Administrator as provided when the '
+  . 'kit was purchased. Please see this person if you have forgotten your password. Otherwise if you choose to have your password reset, '
+  . 'the new password will be sent to the Office Administrator. Likewise if you change the password, the new password will also be sent to the '
+  . 'Office Administrator.<br/>'
+  . 'For any queries call Customer Services on 0800 800 565 (8am - 5pm weekdays)');
+}
+
+function cet_preprocess_user_pass(&$variables) {
+	$variables['pre_text'] = t('<b>Before proceeding:</b> If you are not the administrator of this account, we recommend you contact them before proceeding further '
+	. 'as they will have a record of the current login password.<br/>');
+	$variables['post_text'] = t('An email with a link will be sent to the administrator of this account who will then be requested to reset the password.<br/>'
+	. 'Please check with them for the new password.');
+	
+}
