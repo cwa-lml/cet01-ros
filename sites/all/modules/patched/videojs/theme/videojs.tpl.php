84b5134bd5b34919357835b7238634f51fd282f3
Richard Parratt <rich.d.rich@gmail.com>
2013-05-09 11:20:40 +1200

    #17927 Patch video.js to not preload video

--- sites/all/modules/patched/videojs/theme/videojs.tpl.php
+++ sites/all/modules/patched/videojs/theme/videojs.tpl.php
@@ -12,8 +12,10 @@ if (!empty($poster)) {
   $attrs .= ' poster="'. check_plain($poster) .'"';
 }
 
+// CWA PATCH: Make preload=none for IE9 compatability
 if (!empty($items)): ?>
-<video id="<?php print $player_id; ?>-video" data-setup="{}" class="video-js vjs-default-skin" width="<?php print($width) ?>" height="<?php print($height) ?>" controls="controls" preload="auto"<?php echo $attrs; ?>>
+<video id="<?php print $player_id; ?>-video" data-setup="{}" class="video-js vjs-default-skin" width="<?php print($width) ?>" height="<?php print($height) ?>" 
+	controls="controls" preload="none"<?php echo $attrs; ?>>
 <?php foreach ($items as $item): ?>
   <source src="<?php print(file_create_url($item['uri'])) ?>" type="<?php print check_plain($item['videotype']) ?>" />
 <?php endforeach; ?>
