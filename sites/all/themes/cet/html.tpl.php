0ddc6ad768a9fc6e2c369f1a588e4d88c9fb5679
Richard Parratt <rich.d.rich@gmail.com>
2013-05-11 13:40:46 +1200

    Issue #18759 - force fallback to Flash player on IE9/10, also set the content type for m4v files.

--- sites/all/themes/cet/html.tpl.php
+++ sites/all/themes/cet/html.tpl.php
@@ -11,6 +11,14 @@
   <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
+  <script type="text/javascript" charset="utf-8">
+	// Patch for issue with videojs in IE9/10 - if it is IE9 or IE10 - then fall back to flash
+	if(navigator.userAgent.indexOf("Trident/5")>-1 || navigator.userAgent.indexOf("Trident/6")>-1){
+	 _V_.options.techOrder = ["flash"];
+	 _V_.options.flash.swf = "sites/all/libraries/video-js/video-js.swf";
+	}
+	</script>
+  
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div class="clouds"></div>
